e4f3f932e3fcaa62ea7f63614394893c9ebdbc00
Venkatesh9677 <venkateshvenkey9677@gmail.com>
2023-09-03 20:59:18 +0530

    test coomit

--- MT/src/components/home/About.js
+++ MT/src/components/home/About.js
@@ -12,7 +12,7 @@ const About = () => {
 All scientific evidence and studies prove that being physically active enables you to lead a healthier life. Not just your physical health, but even your emotional and mental well-being is impacted positively when your body is hale and hearty. In the pursuit of holistic wellness, there has been an overall increase across all demographics in the number of people who wish to exercise.
 Several Gyms have mushroomed in and around Chennai. Muscle Trainer Gym in Virugambakkam, Chennai, is a popular choice among fitness enthusiasts in the area. This gym has existed since 2018 and has received an average 4.8 rating. Of course, a gym may help you build muscles and look fabulous, but remember that people who regularly exercise have a lower risk of developing several health concerns, so you might want to check out this place.</p>
 
-{/* <a href="#read-more" className="btn btn-primary nav-button about-btn">READ MORE</a> */}
+{/*<a href="#read-more" className="btn btn-primary nav-button about-btn">READ MORE</a> */}
   </div>
   </div>
  </section>
